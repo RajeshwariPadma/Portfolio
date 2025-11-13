91964bafbe69c66d48104088883778b064047643
RajeshwariPadma <rajeshwaripadma1234@gmail.com>
2025-11-14 00:11:02 +0530

    AboutMe

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,12 @@
 import { Box} from "@mui/material"
 import { Header } from "./Header"
+import { AboutMe } from "./About"
+
 export const Home = () => {
     return (
-        <Box>
+        <Box sx={{ backgroundColor: "#11112cff",}}>
             <Header/>
+            <AboutMe/>
         </Box>
     )
 }
